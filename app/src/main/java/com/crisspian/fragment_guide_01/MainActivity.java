6a1a60572b876cd974a265579eac6ea5e338834a
direyesm <37608963+direyesm@users.noreply.github.com>
2020-08-14 20:13:28 -0400

    ejercicio Horizontal

--- app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -1,5 +1,7 @@
 package com.crisspian.fragment_guide_01;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -14,12 +16,31 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private boolean isFragmentShow = false;
+    public static final String  KEY_ONE = "KEY_ONE";
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putBoolean(KEY_ONE,isFragmentShow);
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        if(savedInstanceState !=null){
+            isFragmentShow = savedInstanceState.getBoolean(KEY_ONE);
+        }
+        if(isFragmentShow){
+            binding.button.setText("Close");
+        }
+
+
+
+
+
 
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
